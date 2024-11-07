ff2a732eef38d753fbf58b5534df8c54614d92af
reffined <alireza.karampoor@gmail.com>
2024-11-07 12:04:00 +0000

    split up methodes

--- appender/appender.go
+++ appender/appender.go
@@ -8,9 +8,27 @@ import (
 	"regexp"
 )
 
-func Append(fileName string, txt []byte, afterType string) ([]byte, error) {
-	pattern := fmt.Sprintf(`(?m)type (?<typeName>%s) struct[\w]?{[\s.\w]*(?<end>})`, afterType)
+func FindEndOfType(fileName string, t string) (int, error) {
+	pattern := fmt.Sprintf(`(?m)type (?<typeName>%s) struct[\w]?{[\s.\w]*(?<end>})`, t)
 	reg := regexp.MustCompile(pattern)
+	file, err := os.Open(fileName)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	content, err := io.ReadAll(file)
+	if err != nil {
+		return 0, err
+	}
+
+	index := reg.FindSubmatchIndex(content)
+	endIndex := reg.SubexpIndex("end")
+	insertPoint := index[endIndex*2] + 1
+	return insertPoint, nil
+}
+
+func Append(fileName string, txt []byte, at int) ([]byte, error) {
 	file, err := os.Open(fileName)
 	if err != nil {
 		return nil, err
@@ -21,12 +39,8 @@ func Append(fileName string, txt []byte, afterType string) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-
-	index := reg.FindSubmatchIndex(content)
-	endIndex := reg.SubexpIndex("end")
-	insertPoint := index[endIndex*2] + 1
-	before := content[:insertPoint]
-	after := content[insertPoint:]
+	before := content[:at]
+	after := content[at:]
 	buf := bytes.Buffer{}
 	buf.Write(before)
 	buf.WriteRune('\n')
